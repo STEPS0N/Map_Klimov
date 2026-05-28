12eb48b31a000baf0e472996b4884f6754a4b8b6
STEPS0N <stepan.klimoff@mail.ru>
2026-05-28 18:42:36 +0500

    Выполненная работа на оценку удовлетворительно. Климов

--- app/src/main/java/com/example/map_klimov/AddressResponse.java
+++ app/src/main/java/com/example/map_klimov/AddressResponse.java
@@ -1,4 +1,25 @@
 package com.example.map_klimov;
 
+import java.util.ArrayList;
+
 public class AddressResponse {
+    public class Response {
+        public class GeoObjectCollection {
+            public class FeatureMember {
+                public class GeoObject {
+                    public class MetaDataProperty {
+                        public class GeocoderMetaData {
+                            public String text;
+                        }
+                        public GeocoderMetaData GeocoderMetaData;
+                    }
+                    public MetaDataProperty metaDataProperty;
+                }
+                public GeoObject GeoObject;
+            }
+            public ArrayList<FeatureMember> featureMember;
+        }
+        public GeoObjectCollection GeoObjectCollection;
+    }
+    public Response response;
 }
